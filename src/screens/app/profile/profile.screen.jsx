e9fb2d1de35d2675b589ce90689f53f996d0c9c1
Danie Schwartz <danie@codehesion.co.za>
2021-02-05 09:03:40 +0000

    profile-screen-styling

--- src/screens/app/profile/profile.screen.jsx
+++ src/screens/app/profile/profile.screen.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { UserInfoForm } from '../../../components/forms';
-import { userModel } from '../../../models';
 import { userService } from '../../../services';
+import useTheme from '../../../theme/hooks/useTheme';
+import FormScreenContainer from '../../../components/containers/form-screen-container/form-screen.container';
 
 const ProfileScreen = () => {
   const { user } = useSelector((reducers) => reducers.userReducer);
   const _onFormSuccess = () => {};
+  const { Gutters } = useTheme();
+
   return (
-    <View>
+    <FormScreenContainer contentContainerStyle={[Gutters.largeMargin]}>
       <UserInfoForm
         edit
         submitForm={userService.updateUser}
         onSuccess={_onFormSuccess}
-        initialValues={userModel(user)}
+        initialValues={user}
       />
-    </View>
+    </FormScreenContainer>
   );
 };
 
