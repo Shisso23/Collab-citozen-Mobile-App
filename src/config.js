81380567236f9f3ab8b8ca67d3076c6cedf02c75
Wesley <wesley@codehesion.co.za>
2021-05-17 21:37:35 +0200

    Refactoring
    - Added correct config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 import Config from 'react-native-config';
 
-const { API_LOCATION, HOST_URL, GOOGLE_MAPS_API_KEY } = Config;
+const { API_LOCATION, HOST_URL, GOOGLE_MAPS_API_KEY, WEB_SITE_URL } = Config;
 
 export default {
   accessTokenKey: 'access_token',
   hostUrl: HOST_URL,
   apiUrl: `${HOST_URL}/${API_LOCATION}`,
   googleMapsApiKey: GOOGLE_MAPS_API_KEY,
-  webSiteUrl: 'https://www.be.co.za',
+  webSiteUrl: WEB_SITE_URL,
 };
