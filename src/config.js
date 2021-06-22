fd4812947fd599df301649eb3b2098d535de4478
hyacinthe <hyacinthe@codehesion.co.za>
2021-06-22 10:19:29 +0200

    added the config file to gitignore

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  accessTokenKey: 'access_token',
-  hostUrl: 'https://citizencollab.collaboratoronline.com',
-  apiUrl: `${'https://citizencollab.collaboratoronline.com'}${'/webapi/Api'}`,
-  googleMapsApiKey: 'AIzaSyCYsg7TyNk3QVYUUzWUrKrkd1T-AuQR934',
-  webSiteUrl: 'https://www.be.co.za',
-  fcmTokenKey: 'fcmToken',
-  fcmEnabled: 'fcmEnabled',
-};
