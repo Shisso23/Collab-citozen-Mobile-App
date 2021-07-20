e5dbf9697e093f3d60e311515e90554a17172a8d
hyacinthe <hyacinthe@codehesion.co.za>
2021-07-20 13:40:59 +0200

    undid changes in config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
+import Config from 'react-native-config';
+
+const { API_LOCATION, HOST_URL, GOOGLE_MAPS_API_KEY, WEB_SITE_URL } = Config;
 export default {
   accessTokenKey: 'access_token',
-  hostUrl: 'https://citizencollab.collaboratoronline.com',
-  apiUrl: `https://citizencollab.collaboratoronline.com${'/webapi/Api'}`,
-  googleMapsApiKey: 'AIzaSyCLLI7N7zC3CHKX2_ptkM0iOGcuNE2if-s',
-  webSiteUrl: 'https://www.be.co.za',
+  hostUrl: HOST_URL,
+  apiUrl: `${HOST_URL}${API_LOCATION}`,
+  googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+  webSiteUrl: WEB_SITE_URL,
   fcmTokenKey: 'fcmToken',
 };
