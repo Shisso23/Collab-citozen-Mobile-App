1ca4a35012121bce49cbc74a3c0db47c4f7e4be8
hyacinthe <hyacinthe@codehesion.co.za>
2021-07-20 08:49:09 +0200

    small fix

--- src/helpers/api-function-name.helper.js
+++ src/helpers/api-function-name.helper.js
@@ -2,7 +2,7 @@ export const apiFunctionWithUniqName = async (functionName) => {
   return {
     taskID: 0,
     uniqName: `${functionName}`,
-    inputValues: '<valRoot><val>codehesion@collaboratoronline.com</val></valRoot>',
+    inputValues: ``,
   };
 };
 
