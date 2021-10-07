de2a7b5179c9c72a9768185db855e8dac5d9715e
hyacinthe <hyacinthe@codehesion.co.za>
2021-10-07 15:08:03 +0200

    added settings Button to manage permissions

--- src/screens/app/profile/profile.screen.jsx
+++ src/screens/app/profile/profile.screen.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Linking, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
+import DeviceInfo from 'react-native-device-info';
+import IntentLauncher from 'react-native-intent-launcher';
+
 import { UserInfoForm } from '../../../components/forms';
 import { userService } from '../../../services';
 import useTheme from '../../../theme/hooks/useTheme';
@@ -13,7 +17,19 @@ const ProfileScreen = () => {
   const _onFormSuccess = () => {
     navigation.navigate('Home');
   };
-  const { Gutters, Fonts } = useTheme();
+  const { Gutters, Fonts, Colors, Layout } = useTheme();
+  const packageName = DeviceInfo.getBundleId();
+
+  const openAppSetting = () => {
+    if (Platform.OS === 'ios') {
+      Linking.openURL('app-settings:');
+    } else {
+      IntentLauncher.startActivity({
+        action: 'android.settings.APPLICATION_DETAILS_SETTINGS',
+        data: `package:${packageName}`,
+      });
+    }
+  };
 
   return (
     <FormScreenContainer contentContainerStyle={[Gutters.smallHMargin, styles.container]}>
@@ -24,6 +40,17 @@ const ProfileScreen = () => {
         onSuccess={_onFormSuccess}
         initialValues={user}
       />
+      <View style={[Layout.alignSelfEnd, Gutters.largeTMargin]}>
+        <Text style={[Fonts.textRegular, styles.permissions]}>Manage your permissions</Text>
+        <Icon
+          name="gear"
+          type="font-awesome"
+          size={55}
+          color={Colors.gray}
+          onPress={openAppSetting}
+          containerStyle={[Layout.alignSelfEnd, Gutters.regularTMargin]}
+        />
+      </View>
     </FormScreenContainer>
   );
 };
@@ -34,6 +61,7 @@ ProfileScreen.defaultProps = {};
 
 const styles = StyleSheet.create({
   container: { paddingBottom: 100 },
+  permissions: { fontWeight: '500' },
 });
 
 export default ProfileScreen;
