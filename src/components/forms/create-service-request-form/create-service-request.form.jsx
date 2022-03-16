9dbcdd3065e271a8dc89a53a9559c5008289cc16
Dylan Krajnc <dylan@codehesion.co.za>
2022-03-16 07:24:38 +0000

    feature/CA-195-new-service-type-selector

--- src/components/forms/create-service-request-form/create-service-request.form.jsx
+++ src/components/forms/create-service-request-form/create-service-request.form.jsx
@@ -1,12 +1,13 @@
-import _ from 'lodash';
 import React, { useEffect, useState, useRef } from 'react';
 import { ViewPropTypes, View, Keyboard } from 'react-native';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { Button, HelperText, TextInput } from 'react-native-paper';
-import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { ListItem, Text } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
+import _ from 'lodash';
 
 import {
   selectChannelSchema,
@@ -17,7 +18,6 @@ import {
 } from '../form-validaton-schemas';
 import { getFormError } from '../form-utils';
 import useTheme from '../../../theme/hooks/useTheme';
-import { DropdownSelect } from '../../atoms';
 import { locationSelector } from '../../../reducers/location-reducer/location.reducer';
 import UploadDocumentButton from '../../molecules/upload-document-button';
 
@@ -32,29 +32,33 @@ const CreateServiceRequestForm = ({
   setThumbNailImages,
   thumbNailImages,
 }) => {
-  const { Common, Layout, Gutters, Colors } = useTheme();
+  const { Common, Layout, Gutters, Colors, Fonts } = useTheme();
   const { selectedAddress, region } = useSelector(locationSelector);
   const [address, setAddress] = useState('');
+  const [searchValue, setSearchValue] = useState('');
   const navigation = useNavigation();
   const formikRef = useRef(null);
-  const [serviceTypesAvailable, setServiceTypesAvailable] = useState(false);
-  const [requirementsExist, setRequirementsExist] = useState(false);
-  const [requirementsServiceType, setRequirementsServiceType] = useState('');
+  const [typeSelected, setTypeSelected] = useState(false);
+  const [isExpandedState, setIsExpandedState] = useState(true);
+  const [noIconState] = useState(true);
+  const [channelChosenName, setChannelChosenName] = useState('');
+  const [categoryChosenName, setCategoryChosenName] = useState('');
+  const [typeChosenName, setTypeChosenName] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const [allTypesStore, setAllTypesStore] = useState([]);
 
-  const checkIfServiceTypesAvaliable = () => {
-    if (Object.keys(municipalities).length >= 1) {
-      const checkMunicipalityCodeRef = Object.keys(municipalities)[0];
-      if (!_.isEmpty(municipalities[checkMunicipalityCodeRef].serviceTypes)) {
-        setServiceTypesAvailable(true);
-      }
-    }
-  };
-
-  useFocusEffect(
-    React.useCallback(() => {
-      checkIfServiceTypesAvaliable();
-    }, []),
-  );
+  useEffect(() => {
+    _.map(Object.keys(municipalities), (municipalityRef) =>
+      Object.keys(municipalities[`${municipalityRef}`].serviceTypes)?.map((categoryName) =>
+        municipalities[`${municipalityRef}`].serviceTypes[`${categoryName}`].map(
+          (serviceTypeObject) => {
+            setAllTypesStore((allTypeStore) => [...allTypeStore, serviceTypeObject]);
+            return null;
+          },
+        ),
+      ),
+    );
+  }, []);
 
   useEffect(() => {
     formikRef.current.setFieldValue('images', thumbNailImages);
@@ -86,6 +90,42 @@ const CreateServiceRequestForm = ({
       .catch((error) => _handleFormSubmitError(error, actions, formData));
   };
 
+  const typeChosen = (channelName, categoryName, typeName) => {
+    setTypeSelected(true);
+    setIsExpandedState(false);
+    setChannelChosenName(channelName);
+    setCategoryChosenName(categoryName);
+    setTypeChosenName(typeName);
+  };
+
+  const changeTypeButtonFunction = () => {
+    setTypeSelected(false);
+    setIsExpandedState(true);
+  };
+
+  const toggleisSearching = () => {
+    setIsSearching(!isSearching);
+  };
+
+  const searchTypes = () => {
+    const newList = [];
+    for (let i = 0; i < allTypesStore.length; i += 1) {
+      if (
+        allTypesStore[i].name.includes(searchValue) ||
+        allTypesStore[i].aliases.indexOf(searchValue.toLowerCase()) >= 0 ||
+        allTypesStore[i].aliases.indexOf(searchValue) >= 0
+      ) {
+        newList.push(allTypesStore[i]);
+      }
+    }
+    return newList;
+  };
+
+  const endSearchFunction = (channelName, categoryName, typeName) => {
+    toggleisSearching();
+    typeChosen(channelName, categoryName, typeName);
+  };
+
   return (
     <View style={containerStyle}>
       <Formik
@@ -105,7 +145,6 @@ const CreateServiceRequestForm = ({
           touched,
           status,
           setFieldValue,
-          setFieldTouched,
         }) => {
           const memoizedChannels = Object.keys(municipalities);
           const channelNames = [];
@@ -122,61 +161,28 @@ const CreateServiceRequestForm = ({
             }
           }, [channelNames]);
 
-          let municipalityCodeRef = null;
-          let memoizedServiceTypeCategories = null;
-          let memoizedServiceTypes = null;
-
-          if (!_.isEmpty(values.channel)) {
-            for (let i = 0; i < Object.keys(municipalities).length; i += 1) {
-              if (values.channel === channelNames[i]) {
-                municipalityCodeRef = memoizedChannels[i];
-              }
-            }
-          }
-
-          if (!_.isEmpty(values.channel) && !_.isNull(municipalityCodeRef)) {
-            memoizedServiceTypeCategories = Object.keys(
-              municipalities[municipalityCodeRef].serviceTypes,
+          const getChannelFromSelectedServiceType = ({ selectedServiceType }) => {
+            return Object.keys(municipalities).find((municipalityRef) =>
+              municipalities[municipalityRef]?.serviceTypes[selectedServiceType.category]?.find(
+                (serviceType) => serviceType.id === selectedServiceType.id,
+              ),
             );
-
-            if (_.isEmpty(memoizedServiceTypeCategories)) {
-              setServiceTypesAvailable(false);
-            } else {
-              setServiceTypesAvailable(true);
-            }
-
-            if (!_.isEmpty(values.serviceTypeCategory)) {
-              memoizedServiceTypes =
-                municipalities[municipalityCodeRef].serviceTypes[values.serviceTypeCategory];
-            }
-          }
-
-          const handleSubmissionFormik = () => {
-            if (serviceTypesAvailable === false) {
-              setFieldValue('serviceTypeCategory', 'No Service Category Avaliable');
-              values.serviceTypeCategory = 'No Service Category Avaliable';
-              setFieldValue('serviceType', { NoServiceType: 'No Service Type Avaliable' });
-              values.serviceType = { NoServiceType: 'No Service Type Avaliable' };
-            }
-            setFieldValue('location', region);
-            values.location = region;
-            setFieldValue('address', selectedAddress);
-            values.address = selectedAddress;
-
-            setFieldValue('municipalityCode', municipalityCodeRef);
-            setFieldValue('channelRef', _.get(municipalities[municipalityCodeRef], 'id', ''));
-            values.municipalityCode = municipalityCodeRef;
-            handleSubmit();
           };
 
-          const setServiceType = (newServiceType) => {
-            setFieldValue('serviceType', newServiceType);
-            if (!_.isEmpty(newServiceType.requirements)) {
-              setRequirementsExist(true);
-              setRequirementsServiceType(newServiceType.requirements);
-            } else {
-              setRequirementsExist(false);
-            }
+          const setFormFields = ({ selectedChannel, selectedServiceType }) => {
+            formikRef.current.setFieldValue('channel', selectedChannel.name);
+            formikRef.current.setFieldValue('serviceTypeCategory', selectedServiceType.category);
+            formikRef.current.setFieldValue('serviceType', selectedServiceType);
+            formikRef.current.setFieldValue('municipalityCode', selectedChannel.municipalityCode);
+            formikRef.current.setFieldValue('channelRef', selectedChannel.id);
+          };
+
+          const handleSubmissionFormik = (vals) => {
+            setFieldValue('location', region);
+            vals.location = region;
+            setFieldValue('address', selectedAddress);
+            vals.address = selectedAddress;
+            handleSubmit();
           };
 
           const error = (name) => getFormError(name, { touched, status, errors });
@@ -190,109 +196,178 @@ const CreateServiceRequestForm = ({
               />
               <HelperText />
 
-              <DropdownSelect
-                items={channelNames}
-                label="Channel"
-                keyExtractor={(item) => item}
-                valueExtractor={(item) => item}
-                onBlur={() => {
-                  setFieldTouched('channel', true);
-                }}
-                placeholder="Channel"
-                onChange={(municipality) => {
-                  setFieldValue('channel', municipality);
-                  setFieldValue('serviceType', null);
-                  setFieldValue('serviceTypeCategory', '');
-                }}
-                value={values.channel}
-                error={error('channel')}
-                errorStyle={Common.errorStyle}
-              />
-
-              {serviceTypesAvailable ? (
-                <DropdownSelect
-                  disabled={_.isEmpty(values.channel)}
-                  items={memoizedServiceTypeCategories}
-                  label="Service Type Category"
-                  keyExtractor={(item) => item}
-                  valueExtractor={(item) => item}
-                  onBlur={() => {
-                    setFieldTouched('serviceTypeCategory', true);
+              {!typeSelected && (
+                <TextInput
+                  value={searchValue}
+                  label="Search..."
+                  underlineColor={Colors.transparent}
+                  onChangeText={(searchText) => {
+                    setSearchValue(searchText);
                   }}
-                  placeholder="Select Service Category"
-                  onChange={(newServiceTypeCategory) => {
-                    setFieldValue('serviceTypeCategory', newServiceTypeCategory);
-                    setFieldValue('serviceType', null);
-                    setRequirementsExist(false);
-                  }}
-                  value={values.serviceTypeCategory}
-                  error={error('serviceTypeCategory')}
-                  errorStyle={Common.errorStyle}
+                  onChange={searchTypes}
+                  onFocus={toggleisSearching}
+                  onBlur={toggleisSearching}
                 />
-              ) : null}
+              )}
+              <HelperText />
 
-              {serviceTypesAvailable ? (
-                <DropdownSelect
-                  disabled={_.isEmpty(values.serviceTypeCategory)}
-                  items={memoizedServiceTypes}
-                  label="Service Type"
-                  keyExtractor={(item) => item.id}
-                  valueExtractor={(item) => item.name}
-                  onBlur={() => {
-                    setFieldTouched('serviceType', true);
+              {!typeSelected && !isSearching && (
+                <>
+                  {Object.keys(municipalities)?.map((municipalityRef) => (
+                    <ListItem.Accordion
+                      key={municipalityRef}
+                      noIcon={noIconState}
+                      underlayColor={Colors.transparent}
+                      content={
+                        <Text style={[Fonts.textRegular]}>
+                          {municipalities[`${municipalityRef}`].name}
+                        </Text>
+                      }
+                      containerStyle={{ backgroundColor: Colors.transparent }}
+                      isExpanded={isExpandedState}
+                    >
+                      {Object.keys(municipalities[`${municipalityRef}`].serviceTypes)?.map(
+                        (categoryName) => (
+                          <ListItem.Accordion
+                            key={categoryName}
+                            noIcon={noIconState}
+                            underlayColor={Colors.transparent}
+                            content={
+                              <Text style={[Fonts.textRegular]}>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{categoryName}
+                              </Text>
+                            }
+                            containerStyle={{ backgroundColor: Colors.transparent }}
+                            isExpanded={isExpandedState}
+                          >
+                            {municipalities[`${municipalityRef}`].serviceTypes[
+                              `${categoryName}`
+                            ].map((serviceTypeObject) => {
+                              return (
+                                <Button
+                                  key={serviceTypeObject}
+                                  mode="contained"
+                                  style={[Layout.fill, Gutters.regularBMargin]}
+                                  onPress={() => {
+                                    typeChosen(
+                                      municipalities[`${municipalityRef}`].name,
+                                      categoryName,
+                                      serviceTypeObject.name,
+                                    );
+                                    setFormFields({
+                                      selectedChannel: municipalities[municipalityRef],
+                                      selectedServiceType: serviceTypeObject,
+                                    });
+                                  }}
+                                >
+                                  {serviceTypeObject.name}
+                                </Button>
+                              );
+                            })}
+                          </ListItem.Accordion>
+                        ),
+                      )}
+                    </ListItem.Accordion>
+                  ))}
+                </>
+              )}
+
+              {typeSelected && !isSearching && (
+                <Button
+                  mode="contained"
+                  style={[Layout.fill, Gutters.regularBMargin]}
+                  onPress={changeTypeButtonFunction}
+                >
+                  Change Type
+                </Button>
+              )}
+
+              {typeSelected && !isSearching && (
+                <Text style={[Fonts.textRegular]}>
+                  Channel: {channelChosenName} {'\n'}
+                  Channel Category: {categoryChosenName} {'\n'}
+                  Selected Type: {typeChosenName} {'\n'}
+                </Text>
+              )}
+
+              {typeSelected && !isSearching && (
+                <TextInput
+                  label="Description"
+                  value={values.description}
+                  multiline
+                  keyboardType="default"
+                  onSubmitEditing={() => {
+                    Keyboard.dismiss();
                   }}
-                  placeholder="Select Service Type"
-                  onChange={(newServiceType) => setServiceType(newServiceType)}
-                  value={values.serviceType?.name}
-                  error={error('serviceType')}
-                  errorStyle={Common.errorStyle}
+                  blurOnSubmit
+                  onBlur={handleBlur('description')}
+                  onChangeText={handleChange('description')}
+                  style={[Common.textInput]}
+                  error={error('description')}
                 />
-              ) : null}
+              )}
 
-              {requirementsExist ? <HelperText>{requirementsServiceType}</HelperText> : null}
-
-              <TextInput
-                label="Description"
-                value={values.description}
-                multiline
-                keyboardType="default"
-                onSubmitEditing={() => {
-                  Keyboard.dismiss();
-                }}
-                blurOnSubmit
-                onBlur={handleBlur('description')}
-                onChangeText={handleChange('description')}
-                style={[Common.textInput]}
-                error={error('description')}
-              />
+              {isSearching &&
+                searchTypes().map((item) => {
+                  const itemChannelGetter = getChannelFromSelectedServiceType({
+                    selectedServiceType: item,
+                  });
+                  return (
+                    <View key={item}>
+                      <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>
+                        {municipalities[itemChannelGetter].name} | {item.category}
+                      </Text>
+                      <Button
+                        mode="contained"
+                        style={[Layout.fill, Gutters.regularBMargin]}
+                        onPress={() => {
+                          const municipality = getChannelFromSelectedServiceType({
+                            selectedServiceType: item,
+                          });
+                          endSearchFunction(
+                            municipalities[municipality].name,
+                            item.category,
+                            item.name,
+                          );
+                          setFormFields({
+                            selectedChannel: municipalities[municipality],
+                            selectedServiceType: item,
+                          });
+                        }}
+                      >
+                        {item.name}
+                      </Button>
+                    </View>
+                  );
+                })}
 
               <HelperText style={Common.errorStyle} type="error" visible={error('description')}>
                 {error('description')}
               </HelperText>
 
-              {requirementsExist ? <HelperText /> : null}
+              {typeSelected && !isSearching && (
+                <View style={[Layout.row]}>
+                  <UploadDocumentButton
+                    title="Take Photo"
+                    style={[Layout.fill, Gutters.tinyRMargin]}
+                    disabled={isSubmitting}
+                    onImageSelect={(images) => {
+                      const uniqueImages = [...new Set([...values.images, ...images])];
+                      setThumbNailImages(uniqueImages);
+                    }}
+                  />
 
-              <View style={[Layout.row]}>
-                <UploadDocumentButton
-                  title="Take Photo"
-                  style={[Layout.fill, Gutters.tinyRMargin]}
-                  disabled={isSubmitting}
-                  onImageSelect={(images) => {
-                    const uniqueImages = [...new Set([...values.images, ...images])];
-                    setThumbNailImages(uniqueImages);
-                  }}
-                />
-
-                <Button
-                  mode="contained"
-                  style={[Layout.fill, Gutters.tinyLMargin]}
-                  onPress={handleSubmissionFormik}
-                  loading={isSubmitting}
-                  disabled={isSubmitting}
-                >
-                  Submit
-                </Button>
-              </View>
+                  <Button
+                    mode="contained"
+                    style={[Layout.fill, Gutters.tinyLMargin]}
+                    onPress={() => handleSubmissionFormik(values)}
+                    loading={isSubmitting}
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </View>
+              )}
             </>
           );
         }}
