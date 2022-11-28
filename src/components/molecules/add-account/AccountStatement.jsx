22c377a61689c6bca732344ce3d0ba00ed8fb06a
Hyacinthe <hyacinthe@codehesion.co.za>
2022-11-28 15:31:00 +0200

    - CA-324
    - Added zoom buttons on the Statements PDF

--- src/components/molecules/add-account/AccountStatement.jsx
+++ src/components/molecules/add-account/AccountStatement.jsx
@@ -1,17 +1,24 @@
-import React, { useMemo } from 'react';
-import { StyleSheet, Dimensions, SafeAreaView } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import { StyleSheet, Dimensions, SafeAreaView, View } from 'react-native';
 import Pdf from 'react-native-pdf';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
+import { Icon } from 'react-native-elements';
 import FlashService from '../../../services/sub-services/flash-service/flash.service';
 import LoadingComponent from '../loading/loading.component';
 import useTheme from '../../../theme/hooks/useTheme';
+import { Colors } from '../../../theme/Variables';
 
 const AccountStatement = ({ statement }) => {
   const { Layout, Gutters } = useTheme();
 
   const source = useMemo(() => _.get(statement, 'statementPdf', {}), []);
+  const [pdfScale, setPdfScale] = useState(1);
+
+  const upDatePdfScale = (value) => () => {
+    setPdfScale(Math.max(1, value));
+  };
 
   return (
     <SafeAreaView style={[Layout.fill, Layout.alignItemsCenter, Layout.justifyContentFlexStart]}>
@@ -20,11 +27,30 @@ const AccountStatement = ({ statement }) => {
         onError={(error) => {
           FlashService.error(_.get(error, 'message', 'Error while loading pdf!'));
         }}
+        scale={pdfScale}
         activityIndicator={<LoadingComponent style={styles.loadingComponent} size="small" />}
         style={[styles.pdf, Gutters.largeBPadding]}
-        fitPolicy={1}
+        fitPolicy={5}
         scrollIndicatorInsets={{ right: 0 }}
-      />
+        maxScale={8}
+        minScale={1}
+      >
+        <View style={[styles.zoomButtons, Gutters.tinyHPadding]}>
+          <Icon
+            name="squared-plus"
+            type="entypo"
+            size={40}
+            onPress={upDatePdfScale(pdfScale + 1)}
+          />
+          <View style={styles.separator} />
+          <Icon
+            name="squared-minus"
+            type="entypo"
+            size={40}
+            onPress={upDatePdfScale(pdfScale - 1)}
+          />
+        </View>
+      </Pdf>
     </SafeAreaView>
   );
 };
@@ -35,6 +61,14 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     height: Dimensions.get('screen').height - Dimensions.get('screen').height * 0.3,
     width: Dimensions.get('screen').width,
+  },
+  separator: { backgroundColor: Colors.gray, height: 1, width: '100%' },
+  zoomButtons: {
+    backgroundColor: Colors.white,
+    bottom: 60,
+    position: 'absolute',
+    right: 10,
+    zIndex: 1,
   },
 });
 
